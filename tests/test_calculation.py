5a996771c9be5ec0453be682716cc81a8bcfecda
Elena <esenvolter2101@gmail.com>
2022-05-19 14:03:16 +0300

    Обновлены тесты

--- tests/test_calculation.py
+++ tests/test_calculation.py
@@ -40,6 +40,32 @@ def test_calc_semi_histogram_performance(calculator):
     assert np.allclose(analytic_data, input_data)
 
 
+def test_calc_semi_array_small_tolerance(calculator):
+    """ Проверка на работоспособность при работе с массивами чисел """
+    print('\n\n** Проверка работоспособности функции calc_semi при работе с массивами очень маленьких чисел. **\n')
+    list_data = (0.0000000362123, 0.0000000305123, 0.0000000286123, 0.0000000389123, 0.0000000479123)
+    print('Введенный массив чисел:' + str(list_data))
+    analytic_data = [3.64323*10 ** -8, 4.68376*10 ** -17, 1.68502*10 ** -25, -2.1064*10 ** -33, 5]
+    print('Результат, рассчитанный аналитически: ' + str(analytic_data))
+    input_data = calculator.calc_semi(list_data)
+    print('Результат работы функции: ' + str(input_data))
+
+    assert np.allclose(analytic_data, input_data)
+
+
+def test_calc_semi_array_big_tolerance(calculator):
+    """ Проверка на работоспособность при работе с массивами чисел """
+    print('\n\n** Проверка работоспособности функции calc_semi при работе с массивами больших чисел. **\n')
+    list_data = [36212349, 30512348, 28612348, 38912348, 47912347, 8912348]
+    print('Введенный массив чисел:' + str(list_data))
+    analytic_data = [95537044/3, 1297969964900003/9, -33813496227444724150000/27, -126756460437225535280000000000/27, 6]
+    print('Результат, рассчитанный аналитически: ' + str(analytic_data))
+    input_data = calculator.calc_semi(list_data)
+    print('Результат работы функции: ' + ", ".join("%.0f" % f for f in input_data))
+
+    assert np.allclose(analytic_data, input_data)
+
+
 def test_calc_semi_array_fault_tolerance(calculator):
     """ Проверка на отказоустойчивость при работе с массивами чисел """
     print('\n\n** Проверка на отказоустойчивость при работе с массивами чисел. **\n')
@@ -62,6 +88,18 @@ def test_calc_semi_histogram_fault_tolerance(calculator):
     print('Результат работы функции: ' + str(input_data))
 
     assert input_data == ErrorCodes.ERROR_STR_HISTOGRAM
+
+
+def test_calc_semi_histogram_fault_str_tolerance(calculator):
+    """ Проверка на отказоустойчивость при работе с гистограммами """
+    print('\n\n** Проверка на отказоустойчивость при работе с гистограммами. **\n')
+    list_data = [[2, 0], (3, 5), [9, 14], [5, 6]]
+    print('Введенная гистограмма чисел: ' + str(list_data))
+    print('Ожидаемый результат: ErrorCodes.ERROR_STR_ARRAY.')
+    input_data = calculator.calc_semi(list_data)
+    print('Результат работы функции: ' + str(input_data))
+
+    assert input_data == ErrorCodes.ERROR_STR_ARRAY
 
 
 def test_calc_semi_histogram_type_fault_tolerance(calculator):
@@ -181,4 +219,6 @@ def test_calc_three_points_probability_fault_tolerance(calculator):
     print('Результат работы функции: ' + str(input_data))
 
     assert input_data == ErrorCodes.ERROR_PROBABILITY_NOT_1
+
+
 
